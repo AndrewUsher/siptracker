63f258425c254e6b61f17d7495b675bb2d140910
Andrew Usher <andrewusher00@gmail.com>
2025-04-23 20:16:27 -0500

    refactor: replace ThemeProvider and ReactQueryProvider with a new Providers component for better organization

---
+++ app/providers.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  })
+}
+
+let browserQueryClient: QueryClient | undefined = undefined
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient()
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient()
+    return browserQueryClient
+  }
+}
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient()
+
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </NextThemesProvider>
+  )
+}
